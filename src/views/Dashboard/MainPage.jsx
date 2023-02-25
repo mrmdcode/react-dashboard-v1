6f0301fbc46fd627f254731f88280002c6f04fdc
mahdi kazemi <dr.mahdikazemizade84@gmail.com>
2023-02-25 21:45:34 +0330

    create first part of analyze-box in MainPage.jsx

--- src/views/Dashboard/MainPage.jsx
+++ src/views/Dashboard/MainPage.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import {BiDotsVerticalRounded} from 'react-icons/bi';
 
 const MainPage = () => {
 
 return(
-    <div>main-dashboard</div>
+    <div className="main-container">
+        <div className="header-page">
+            <div className="analyze-container">
+                <div className="analyze-box">
+                    <div className="header-box">
+                        <div className="title">New User</div>
+                        <div className="operation">
+                            <BiDotsVerticalRounded/>
+                        </div>
+                    </div>
+                    <div className="content-box">
+                        <div className="circle-chart">
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div className="chart-detail">
+                            2550
+                        </div>
+                    </div>
+                </div>
+                <div className="analyze-box">analizesssss</div>
+                <div className="analyze-box">analizesssss</div>
+                <div className="analyze-box">analizesssss</div>
+            </div>
+        </div>
+    </div>
 )
 }
 export default MainPage;
