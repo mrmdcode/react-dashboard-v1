adc85459392e643dab07c74a806dcd0b9347284d
mahdi kazemi <dr.mahdikazemizade84@gmail.com>
2023-02-25 18:56:16 +0330

    little change

--- src/views/test_1.jsx
+++ src/views/test_1.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {useStateContext} from "../context/ContextProvider";
 
 const Test1 = () => {
-    const {user} = useStateContext();
+   
     return (
         <div>
             <h3>say hello</h3>
-            {user}
+
         </div>
     )
 }
