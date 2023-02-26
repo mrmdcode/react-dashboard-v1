8ec37b517ca9bdd9984d8f2df2028dec4c330579
mahdi kazemi <dr.mahdikazemizade84@gmail.com>
2023-02-26 23:46:56 +0330

    upgrate list-users.css and ListUserItem.jsx and create fake data

--- src/components/dashboard/ListUserItem.jsx
+++ src/components/dashboard/ListUserItem.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
+import {AiOutlineUserDelete ,AiOutlineEdit ,AiOutlineKey} from 'react-icons/ai';
 
-const ListUserItem = ({active}) => {
+
+const ListUserItem = ({index ,active =false , data ,OperationHandler}) => {
+    const {
+        id,
+        FirstName,
+        LastName,
+        Email,
+        Skill,
+        Skills,
+        TodoCount,
+        DoneProjectCount,
+        WorkingOnCount,
+        Roles,
+        RegisterAt,
+        LastLogin
+    } = data;
 
     return (
         <tr className={active ? "active-row" : null}>
-            <td>1</td>
-            <td>mahdi kazemi</td>
-            <td>mdka2885mdka@gmail.com</td>
-            <td>PHP , react ,...</td>
-            <td>5</td>
-            <td>2</td>
-            <td>2</td>
-            <td>Admin , Manager ,Senior , ...</td>
+            <td>{index}</td>
+            <td>{FirstName + LastName}</td>
+            <td>{Email}</td>
+            <td>{Skill}</td>
+            <td>{Skills[0]} , {Skills[1]}, {Skills[2] ?`${Skills[2]} ,` : null } ...</td>
+            <td>{TodoCount}</td>
+            <td>{DoneProjectCount}</td>
+            <td>{WorkingOnCount}</td>
+            <td>{Roles[0]} ,  {Roles[1] ?`${Roles[1]} ,` : null } {Roles[2] ?`${Roles[2]} ` : null } ...</td>
+            <td>{RegisterAt}</td>
+            <td>{LastLogin}</td>
             <td className="operation">
                 <span className="row">
-                    <button>Delete</button>
-                    <button>Edit</button>
+                    <button onClick={()=>OperationHandler(id,"Delete")}><AiOutlineUserDelete/> Delete</button>
+                    <button onClick={()=>OperationHandler(id,"Edit")}><AiOutlineEdit/> Edit</button>
                 </span>
                 <span className="row">
-                    <button>Fast Edit</button>
-                    <button>Reset Password</button>
+                    <button onClick={()=>OperationHandler(id,"FastEdit")}><AiOutlineEdit/> Fast Edit</button>
+                    <button onClick={()=>OperationHandler(id,"ResetPassword")}><AiOutlineKey/>Reset Password</button>
                 </span>
             </td>
         </tr>
--- src/views/Dashboard/ListUsers.jsx
+++ src/views/Dashboard/ListUsers.jsx
@@ -2,8 +2,54 @@ import React from 'react';
 import ListUserItem from "../../components/dashboard/ListUserItem";
 
 const ListUsers = () => {
+    const users = [
+        {
+            id:1,
+            FirstName : "mahdi",
+            LastName : "kazemi zade",
+            Email : "mdka2885mdka@gmail.com",
+            Skill : "Backend Developer",
+            Skills : ["PHP","React","Js","wordpress"],
+            TodoCount : 5,
+            DoneProjectCount : 2,
+            WorkingOnCount : 5,
+            Roles : ["Admin","Manager" ,"Senior"],
+            RegisterAt : "1399/12/29",
+            LastLogin : "1401/11/29"
+        },
+        {
+            id:2,
+            FirstName : "mamd",
+            LastName : "hosseini",
+            Email : "mamad.hossein@gmail.com",
+            Skill : "Front Developer",
+            Skills : ["React","Js","TypeScript"],
+            TodoCount : 1,
+            DoneProjectCount : 1,
+            WorkingOnCount : 1,
+            Roles : ["Programmer" , "Writer"],
+            RegisterAt : "1399/12/29",
+            LastLogin : "1401/11/29"
+        },{
+            id:1,
+            FirstName : "ali",
+            LastName : "sirabi",
+            Email : "ali.aliee@gmail.com",
+            Skill : "Python Developer",
+            Skills : ["django" , "machine learninig"],
+            TodoCount : 3,
+            DoneProjectCount : 3,
+            WorkingOnCount : 3,
+            Roles : ["Programmer" ,"Writer"],
+            RegisterAt : "1399/12/29",
+            LastLogin : "1401/11/29"
+        }
+    ]
 
 
+    const OperationHandler =(Chosse , id)=>{
+        console.log(Chosse , id)
+    }
     return (
         <div className="list-user">
             <section className="list row">
@@ -13,20 +59,23 @@ const ListUsers = () => {
                             <th>#</th>
                             <th>Full Name</th>
                             <th>Email</th>
+                            <th>Skill</th>
                             <th>Skills</th>
                             <th>Todo Count</th>
                             <th>Done Project Count</th>
                             <th>Working On</th>
                             <th>Role</th>
+                            <th>Register At</th>
+                            <th>Last Login At</th>
                             <th>Operations</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <ListUserItem active={true}/>
-                        <ListUserItem/>
-                        <ListUserItem/>
-                        <ListUserItem/>
-                        <ListUserItem/>
+                        {users.map((user ,index)=>{
+                            return(
+                                <ListUserItem key={index} index={index} data={user} OperationHandler={OperationHandler}/>
+                            );
+                        })}
                     </tbody>
                 </table>
             </section>
