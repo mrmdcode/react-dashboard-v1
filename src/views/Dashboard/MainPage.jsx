f7205d81256739d4c274a244a630334f8ef58573
mahdi kazemi <dr.mahdikazemizade84@gmail.com>
2023-02-27 19:12:50 +0330

    create chart-box and user-suggestion-box

--- src/views/Dashboard/MainPage.jsx
+++ src/views/Dashboard/MainPage.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-
+import { Line } from 'react-chartjs-2';
 import {BarAnalyzeBox, CircleAnalyzeBox} from "../../components/dashboard/AnalyzeBox";
 const MainPage = () => {
 
 return(
     <div className="main-container">
-        <div className="header-page">
-            <div className="analyze-container">
-                <CircleAnalyzeBox name="Maximum Register Daily" value="60" mark="User">500</CircleAnalyzeBox>
-                <BarAnalyzeBox name="New Mails" value={20}>1500</BarAnalyzeBox>
-                <CircleAnalyzeBox name="Today Login User" value="6" mark="User">50</CircleAnalyzeBox>
-                <BarAnalyzeBox name="Response Requests" value={100}>190</BarAnalyzeBox>
-
-            </div>
+        <div className="analyze-container">
+            <CircleAnalyzeBox name="Maximum Register Daily" value="60" mark="User">500</CircleAnalyzeBox>
+            <BarAnalyzeBox name="New Mails" value={20}>1500</BarAnalyzeBox>
+            <CircleAnalyzeBox name="Today Login User" value="6" mark="User">50</CircleAnalyzeBox>
+            <BarAnalyzeBox name="Response Requests" value={100}>190</BarAnalyzeBox>
+        </div>
+        <div className="chart-container">
+            <div className="chart-box"></div>
+            <div className="chart-box"></div>
+            <div className="chart-box"></div>
+        </div>
+        <div className="user-suggestion-box">
+            <div className="user-box"></div>
+            <div className="user-box"></div>
+            <div className="user-box"></div>
+            <div className="user-box"></div>
         </div>
     </div>
 )
