02533970c8ed9f954179311d55aefef74640f8fb
mahdi kazemi <dr.mahdikazemizade84@gmail.com>
2023-03-03 14:47:37 +0330

    fix color for Skill in user-suggestion-box

--- src/components/dashboard/SuggestionUserBox.jsx
+++ src/components/dashboard/SuggestionUserBox.jsx
@@ -3,13 +3,17 @@ import {useStateContext} from "./../../context/ContextProvider"
 
 const SuggestionUserBox = () => {
     const {appUrl} = useStateContext();
+    const SkillColor = ["c-1","c-2","c-3","c-4"];
+    const MinNumber = 0;
+    const MaxNumber = 3;
+    const RandomNumber = MinNumber + (Math.random() * (MaxNumber - MinNumber));
     return (
         <div className="user-box">
             <img src={appUrl+"/user/1.jpg"} alt="user 1" className="user-img"/>
             <div className="user-detail">
                 <p className="user-name">Mahdi Kazemi</p>
                 <p className="user-email">dr.mahdikazemizade@gmail.com</p>
-                <p className="user-skill c-1">Backend Developer</p>
+                <p className={"user-skill " + SkillColor[Math.round(RandomNumber)]}>Backend Developer</p>
             </div>
             <span className="user-busy"></span>
         </div>
