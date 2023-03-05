84e0334182ae44c5dcffca65d27f8e0657f91560
mahdi kazemi <dr.mahdikazemizade84@gmail.com>
2023-03-05 11:57:07 +0330

    working on add login page

--- src/views/outher/Login.jsx
+++ src/views/outher/Login.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {useStateContext} from "../../context/ContextProvider";
 import Background from "./../../assets/img/Login-Page-Background.jpg"
 import {SlSocialFacebook, SlSocialGithub, SlSocialGoogle, SlSocialInstagram, SlSocialLinkedin} from "react-icons/sl";
 
 const Login = () => {
     const {setTitle} =useStateContext();
-    setTitle("Login Page")
+    const [loginData, setLoginData] = useState({
+        username:"",
+        password:"",
+        RemmberMe:false,
+        ExitAfterClose:false,
+    });
+    const HandlerInputLoginData = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setLoginData({...loginData,[name]:value});
+    }
+    const HandlercheckboxLoginData = (e) => {
+        const name = e.target.name;
+        const checked = e.target.checked;
+        setLoginData({...loginData,[name]:checked});
+
+    }
+    const HandlerSendData = () => {
+        console.log(loginData)
+        setLoginData({
+            username:"",
+            password:"",
+            RemmberMe:false,
+            ExitAfterClose:false,
+        });
+        
+    }
+        setTitle("Login Page")
     return (
         <div className="login-page" style={{backgroundImage:`url(${Background})`}}>
             <div className="login-container">
@@ -17,19 +44,19 @@ const Login = () => {
                     <li><SlSocialLinkedin/></li>
                 </ul>
                 <div className="content">
-                    <input type="text" name="Username" id="UserName" placeholder="username"/>
-                    <input type="password" name="password" id="password" placeholder="password"/>
+                    <input type="text" name="username" id="username" placeholder="username" onChange={HandlerInputLoginData} value={loginData.username}/>
+                    <input type="password" name="password" id="password" placeholder="password" onChange={HandlerInputLoginData} value={loginData.username}/>
                     <span>
                         <span>
-                            <label for="RemmberMe">remmber me</label>
-                            <input type="checkbox" name="RemmberMe" id="RemmberMe"/>
+                            <label htmlFor="RemmberMe">remmber me</label>
+                            <input type="checkbox" name="RemmberMe" id="RemmberMe" onChange={HandlercheckboxLoginData} checked={loginData.RemmberMe}/>
                         </span>
                         <span>
                             <label htmlFor="ExitAfterClose">exit after close</label>
-                            <input type="checkbox" name="ExitAfterClose" id="ExitAfterClose"/>
+                            <input type="checkbox" name="ExitAfterClose" id="ExitAfterClose" onChange={HandlercheckboxLoginData} checked={loginData.ExitAfterClose}/>
                         </span>
                     </span>
-                    <button>
+                    <button onClick={HandlerSendData}>
                         Login
                     </button>
                 </div>
